7fbcf4e8e586a0fb1e8356b0767af800dad1f45c
chisumo <bchisumo74@yahoo.com>
2021-05-18 23:31:48 +0100

    Live search functionality added by livewire component

--- app/Http/Livewire/Documents.php
+++ app/Http/Livewire/Documents.php
@@ -9,11 +9,21 @@ use Livewire\WithPagination;
 class Documents extends Component
 {
     use WithPagination;
+
+    public  $term;
+
+    protected  $queryString = ['term'];
+
+
     public function render()
     {
-        $documents = Document::with('user')
-            ->orderBy('created_at','DESC')
-            ->paginate(2);
+        if($this->term){
+            $documents = Document::search($this->term)->paginate(3);
+        }else{
+            $documents = Document::with('user')
+                ->orderBy('created_at','DESC')
+                ->paginate(2);
+        }
         return view('livewire.documents',compact('documents'));
     }
 }
--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -5,26 +5,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 flex justify-between">
-                    <form action="{{ route('documents.search') }}" method="GET">
-{{--                        @csrf--}}
-                        <x-input class="ml-3 border-b py-2 text-grey-darkest shadow-none" name="term">
-
-                        </x-input>
-                        <x-button class="ml-3">Search</x-button>
-                    </form>
-                    <a href="{{ route('documents.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25
-                     transition ease-in-out duration-150">Upload
-                    </a>
-                </div>
-            </div>
-        </div>
 {{--       <x-document-table :documents="$documents" />--}}
         <livewire:documents />
-
     </div>
 
     <div class="py-12">
